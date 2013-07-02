176ffd5fffde0e31fc0af78c54b34a33b77bc60a
Roel <ik.roelbaars@gmail.com>
2013-07-02 10:03:46 +0200

    Layout updated to new version + ckeditor added

---
+++ static/ckeditor/config.js
@@ -0,0 +1,14 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {	
+	config.forcePasteAsPlainText = true;
+	config.toolbar = [
+    [ 'Bold', 'Italic', 'Subscript', 'Superscript' ]
+];
+
+};
+
+
--- static/jury.js
+++ static/jury.js
@@ -1,2 +1,10 @@
 function gid(element) { return document.getElementById(element); }
 
+function enlarge(obj) {
+	if (obj.className != '')
+		obj.className = '';
+	else
+		obj.className = 'large';
+}
+
+

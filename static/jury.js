a415ede7359545f3c4632d1685def480e85f5c48
Roel Baars <ik.roelbaars@gmail.com>
2013-07-04 18:17:13 +0200

    Login styled, profile page styled, css changes, some conceptual js

--- static/jury.js
+++ static/jury.js
@@ -7,4 +7,31 @@ function enlarge(obj) {
 		obj.className = 'large';
 }
 
+function SelectAllToggle() {
+	var l = document.getElementsByName('pdfselect');
+	if (l[0].checked == true) {
+		for (var i=0;i<l.length;i++) {
+			l[i].checked = false;
+		}
+	} else {
+		for(var i=0;i<l.length;i++) {
+			l[i].checked = true;
+		}
+	}
+}
 
+function DownloadPDF() {
+	var l = document.getElementsByName('pdfselect');
+	var s = 'Make PDF of questions: ';
+	var c = 0;
+	for (var i=0;i<l.length;i++) {
+		if (l[i].checked) {
+			s = s + l[i].value + ' ';
+			c++;
+		}
+	}
+	if (c != 0)
+		confirm(s);
+	else
+		alert('No questions selected!');
+}

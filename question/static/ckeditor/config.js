aeeef83b350401628401279304198afecc34b17c
Roel Baars <ik.roelbaars@gmail.com>
2013-07-10 14:54:13 +0200

    Fixes for CKEditor (font size, placement)

--- question/static/ckeditor/config.js
+++ question/static/ckeditor/config.js
@@ -3,12 +3,15 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {	
+CKEDITOR.editorConfig = function( config ) {
 	config.forcePasteAsPlainText = true;
 	config.toolbar = [
     [ 'Bold', 'Italic', 'Subscript', 'Superscript' ]
-];
-
+	];
+	config.removePlugins = 'elementspath';
+	config.resize_enabled = false;
+	config.extraPlugins = 'autogrow';
+	config.autoGrow_maxHeight = 0;
 };
 
 

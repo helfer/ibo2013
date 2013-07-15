13035aaba0d4dea7965545e06982bd76c552cb27
Roel Baars <ik.roelbaars@gmail.com>
2013-07-16 00:39:42 +0200

    Quickfix. It works!

--- question/static/students.js
+++ question/static/students.js
@@ -190,7 +190,6 @@ function save_answer(qid,ans,ename) {
 // fix for images now showing up immediately
 window.onload = function() {
 	var v = document.getElementsByTagName('img');
-	alert(v.length);
 for (var i = 0; i < v.length; i++) {
 	v[i].style.display = 'none';
 	v[i].style.display = 'block';

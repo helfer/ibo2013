f3c0ecc637656e6560336711c474af1abd51085c
helfer <helfer@mit.edu>
2013-08-12 03:34:16 +0200

    tscore?

--- tscore.py
+++ tscore.py
@@ -9,15 +9,18 @@ fs = FinalScore.objects.filter(~Q(id=141))
 sums = [0]*4
 zerocount = 0
 
-count = 0
+count = 0.0
+for f in fs:
+    if f.auth_user_id in [141,39,303,132]:
+	print "delete",f.auth_user_id
+	f.delete()
+	
+fs = FinalScore.objects.all()
 for f in fs:
     count += 1
     for i in range(1,5):
         score = getattr(f,"p{0}".format(i))
-        if score == 0:
-            count -= 1
-            zerocount += 1
-            continue
+	assert score != 0
         sums[i-1] += float(score)
 
 print 'zero',zerocount
@@ -30,12 +33,10 @@ sds = [0]*4
 for f in fs:
     for i in range(1,5):
         score = float(getattr(f,"p{0}".format(i)))
-        if score == 0:
-            continue
         sds[i-1] += (avgs[i-1] - score)**2
 print 'count',count
 print 'before divide',sds
-sds = map(lambda x:x/count,sds)
+sds = map(lambda x:x/(count-1),sds)
 print 'sds after divide',sds
 sds = map(math.sqrt,sds)
 print 'sds final',sds
@@ -45,16 +46,21 @@ var = [0]*4
 for f in fs:
     for i in range(1,5):
         score = float(getattr(f,"p{0}".format(i)))
-        if score == 0:
-            continue
         tscore = (score-avgs[i-1])/sds[i-1]
         zums[i-1] += tscore
         setattr(f,"t{0}".format(i),tscore)
         
     #f.save()
 
-
+vrs = [0]*4
+for f in fs:
+    for i in range(1,5):
+        tscore = float(getattr(f,"t{0}".format(i)))
+        vrs[i-1] += (tscore**2)/count
+    print f.id
+    f.save()
 print "sums",zums
+print "var",vrs	
 print "done"
 
         
